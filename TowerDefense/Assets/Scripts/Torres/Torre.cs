ffa839e35ddaa4e432ed42c7c46420fa2cf0f13b
semimarufbi <gabicolzani@gmail.com>
2024-10-28 14:53:15 -0300

    fazendooscripttorre

--- TowerDefense/Assets/Scripts/Torres/Torre.cs
+++ TowerDefense/Assets/Scripts/Torres/Torre.cs
@@ -6,21 +6,40 @@ using UnityEngine;
 public class Torre : MonoBehaviour
 {
     [SerializeField] private float alvoNoRaio = 5f;
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] private Transform pontoDeTiro;
+    [SerializeField] private float bps = 1f;//balas por segundo
+    [SerializeField] private LayerMask inimigoMask;
+    private Transform alvo;
+    private float tempoEntreBalas;
 
+
+    private void Update()
+    {
+        if(alvo == null)
+        {
+            FindTarget();
+            return;
+        }
+        ChecarAlvoEstaNoAlcance();
+
+
+    }   
+
+    private void FindTarget()
+    {
     
+         RaycastHit2D[] hits = Physics2D.CircleCastAll(transform.position, alvoNoRaio, (Vector2)transform.position, 0f, inimigoMask);
 
-
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        if(hits.Length > 0 )
+        {
+            alvo = hits[0].transform;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private bool ChecarAlvoEstaNoAlcance()
     {
-        
+        return Vector2.Distance(alvo.position, transform.position) <= alvoNoRaio;
     }
+
 }
