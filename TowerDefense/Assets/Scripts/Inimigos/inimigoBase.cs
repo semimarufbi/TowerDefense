f2bee97dbf3fc2a437eb36d548e8f832356dcc8b
semimarufbi <152785706+semimarufbi@users.noreply.github.com>
2024-10-31 16:38:22 -0300

    Revert "arruamndoamoeda"
    This reverts commit f022ea7c0e9546543cd69862a08887a0bb318eff.

--- TowerDefense/Assets/Scripts/Inimigos/inimigoBase.cs
+++ TowerDefense/Assets/Scripts/Inimigos/inimigoBase.cs
@@ -6,7 +6,7 @@ public class inimigoBase : MonoBehaviour, IReceberDano
 {
     [Header("Atributos")]
     [SerializeField] protected float moveSpeed = 100f; // Velocidade de movimento do inimigo
-    [SerializeField] private int currentWorth = 50; // Quantidade de moeda que este inimigo vale
+    [SerializeField] private int currentWorth = 50;
 
     [Header("Referências")]
     [SerializeField] protected Rigidbody2D rb; // Referência ao componente Rigidbody2D para controle de física
@@ -14,17 +14,16 @@ public class inimigoBase : MonoBehaviour, IReceberDano
     protected Transform alvo; // Posição do alvo atual no caminho
     protected int pathIndex = 0; // Índice do ponto atual no caminho
 
-    [SerializeField] public int vidaAtual = 100; // Vida inicial do inimigo
+    [SerializeField] public int vidaAtual = 100; // Vida inicial do inimigo, usada somente no método `ReceberDano`
 
     // Método de interface que reduz a vida e verifica se o inimigo morreu
     public virtual void ReceberDano(int dano)
     {
-        vidaAtual -= dano; // Reduz a vida do inimigo pelo dano recebido
-
-        // Verifica se a vida do inimigo chegou a zero ou menos
+        vidaAtual -= dano;
+        LevelManager.main.IncreaseCurerency(currentWorth);
         if (vidaAtual <= 0)
         {
-            OnMorte(); // Chama o método de morte se a vida for igual ou menor que zero
+            OnMorte();
         }
     }
 
@@ -57,18 +56,17 @@ public class inimigoBase : MonoBehaviour, IReceberDano
         pathIndex++;
         if (pathIndex >= LevelManager.main.path.Length)
         {
-            OnMorte(); // Chama o método de morte se o inimigo atingir o final do caminho
+            OnMorte();
         }
         else
         {
-            alvo = LevelManager.main.path[pathIndex]; // Atualiza o alvo para o próximo ponto
+            alvo = LevelManager.main.path[pathIndex];
         }
     }
 
     public virtual void OnMorte()
     {
-        LevelManager.main.IncreaseCurerency(currentWorth); // Aumenta a moeda ao morrer
-        EnemySpawner.onEnemyDestroy.Invoke(); // Invoca o evento de inimigo destruído
-        Destroy(gameObject); // Destrói o objeto inimigo
+        EnemySpawner.onEnemyDestroy.Invoke();
+        Destroy(gameObject);
     }
 }
