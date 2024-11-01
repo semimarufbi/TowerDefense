b4ced3d838a50e785c9023fcc7fcd1a5ccb63f04
semimarufbi <152785706+semimarufbi@users.noreply.github.com>
2024-10-31 22:44:39 -0300

    levelmanagercomentado

--- TowerDefense/Assets/Scripts/LevelManager.cs
+++ TowerDefense/Assets/Scripts/LevelManager.cs
@@ -2,38 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// Classe responsável pela gestão do nível, incluindo caminho e moedas
 public class LevelManager : MonoBehaviour
 {
-    public static LevelManager main;
-    public Transform[] path;
-    public Transform startPoint;
+    public static LevelManager main; // Instância estática para acesso global
+    public Transform[] path; // Array que armazena os pontos do caminho
+    public Transform startPoint; // Ponto inicial do caminho
 
-    public int currency;
+    public int currency; // Moeda do jogador
 
     private void Awake()
     {
-        main = this;
-    }
-    private void Start()
-    {
-        currency = 100;
+        main = this; // Inicializa a instância estática
     }
 
+    private void Start()
+    {
+        currency = 100; // Define a moeda inicial do jogador
+    }
+
+    // Método para aumentar a quantidade de moeda
     public void IncreaseCurerency(int amanunt)
     {
         currency += amanunt;
     }
 
-    public bool SpendCurency(int amanunt) 
+    // Método para gastar moeda, retorna verdadeiro se a transação foi bem-sucedida
+    public bool SpendCurency(int amanunt)
     {
-        if (amanunt <= currency)
+        if (amanunt <= currency) // Verifica se há moeda suficiente
         {
-            currency -= amanunt;
-            return true;
+            currency -= amanunt; // Deduz a quantidade gasta
+            return true; // Retorna verdadeiro se a transação foi bem-sucedida
         }
         else
         {
-            return false;
+            return false; // Retorna falso se não há moeda suficiente
         }
     }
 }
