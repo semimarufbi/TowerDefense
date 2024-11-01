f0c743cf82239071a95eb11189c9b3a1515113bb
semimarufbi <152785706+semimarufbi@users.noreply.github.com>
2024-10-31 22:45:30 -0300

    towercomentado

--- TowerDefense/Assets/Scripts/Torres/Tower.cs
+++ TowerDefense/Assets/Scripts/Torres/Tower.cs
@@ -2,13 +2,14 @@ using System;
 using UnityEngine;
 
 [Serializable]
-public class Tower 
+public class Tower
 {
-    public string name;
-    public int coast;
-    public GameObject prefab;
+    public string name; // Nome da torre
+    public int coast; // Custo da torre
+    public GameObject prefab; // Prefab associado à torre
 
-    public Tower (string _name, int _coast, GameObject _prefab)
+    // Construtor para inicializar a torre com nome, custo e prefab
+    public Tower(string _name, int _coast, GameObject _prefab)
     {
         this.name = _name;
         this.coast = _coast;
