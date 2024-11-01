8a035a43d7387d22dfcccc24f397581f391e69af
semimarufbi <152785706+semimarufbi@users.noreply.github.com>
2024-10-31 22:34:40 -0300

    buildmanagercomentado

--- TowerDefense/Assets/Scripts/BuildManager.cs
+++ TowerDefense/Assets/Scripts/BuildManager.cs
@@ -4,30 +4,30 @@ using UnityEngine;
 
 public class BuildManager : MonoBehaviour
 {
+    // Instância única do BuildManager (Singleton)
     public static BuildManager Instance;
 
     [Header("References")]
-    [SerializeField] private Tower[] towers;
+    [SerializeField] private Tower[] towers; // Array de torres disponíveis para construção
 
     [Header("Attributes")]
+    private int selectedTower = 0; // Índice da torre selecionada atualmente
 
-    private int selectedTower = 0;
-
-
-
+    // Método chamado na inicialização do objeto
     private void Awake()
     {
-        Instance = this;
+        Instance = this; // Define a instância do BuildManager
     }
 
+    // Método para obter a torre atualmente selecionada
     public Tower GetSelectedTower()
     {
-        return towers[selectedTower];
+        return towers[selectedTower]; // Retorna a torre com base no índice selecionado
     }
 
+    // Método para definir a torre selecionada
     public void SetSelectedTower(int _selectedTower)
     {
-        selectedTower = _selectedTower;
+        selectedTower = _selectedTower; // Atualiza o índice da torre selecionada
     }
 }
-
