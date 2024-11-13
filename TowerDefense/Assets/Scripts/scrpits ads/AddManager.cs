3adbeef8168ca5628bd4b50b28d074eea0ed8ee6
semimarufbi <gabicolzani@gmail.com>
2024-11-13 13:51:55 -0300

    metodosiniciais

--- TowerDefense/Assets/Scripts/scrpits ads/AddManager.cs
+++ TowerDefense/Assets/Scripts/scrpits ads/AddManager.cs
@@ -3,17 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Advertisements;
 
-public class AddManager : MonoBehaviour
+public class AddManager : MonoBehaviour,IUnityAdsInitializationListener
 {
-    
+    private string gameid = "5730170";
+    private bool testmode = true;
+
+
     void Start()
     {
-        
+        InitializeAds();
+    }
+    private void InitializeAds()
+    {
+        if (!Advertisement.isInitialized)
+        {
+            Advertisement.Initialize(gameid, testmode, this);
+        }
+    }
+    public void OnInitializationComplete()
+    {
+        Debug.Log("Unity Ads initialization complete.");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnInitializationFailed(UnityAdsInitializationError error, string message)
     {
         
     }
+    
 }
